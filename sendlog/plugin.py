08e5a6259cfb3828f11bce5c65921ba848b06e0a
samcole8 <sam@samcole.me>
2025-02-23 19:54:37 +0000

    feat: recursively run workflows with `execute()`

--- sendlog/plugin.py
+++ sendlog/plugin.py
@@ -12,3 +12,48 @@ class _WorkflowNode:
 
     def __iter__(self):
         return iter(self.children)
+
+class _LogMeta(type):
+    def __new__(cls, name, bases, dct):
+        return super().__new__(cls, name, bases, dct)
+
+class _RuleMeta(type):
+    def __new__(cls, name, bases, dct):
+        return super().__new__(cls, name, bases, dct)
+
+class _TransformationMeta(type):
+    def __new__(cls, name, bases, dct):
+        return super().__new__(cls, name, bases, dct)
+
+class _EndpointMeta(type):
+    def __new__(cls, name, bases, dct):
+        return super().__new__(cls, name, bases, dct)
+
+class Log(_WorkflowNode, metaclass=_LogMeta):
+    def execute(self, line):
+        for rule in self.children:
+            rule.execute(line)
+
+class Rule(_WorkflowNode, metaclass=_RuleMeta):
+    def execute(self, line):
+        if self.evaluate(line) is True:
+            for transformation in self.children:
+                transformation.execute(line)
+    
+    def evaluate(self, line):
+        raise NotImplementedError
+
+class Transformation(_WorkflowNode, metaclass=_TransformationMeta):
+    def execute(self, line):
+        msg = self.transform(line)
+        for endpoint in self.children:
+            endpoint.send(msg)
+
+    def transform(self, line):
+        raise NotImplementedError
+
+class Endpoint(_WorkflowNode, metaclass=_EndpointMeta):
+    def send(self, msg):
+        raise NotImplementedError
+
+__all__ = ["Log", "Rule", "Transformation", "Endpoint"]
