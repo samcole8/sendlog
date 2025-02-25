dcf6d1475f2f46601bb32c9f1a48a864f3f6670c
samcole8 <sam@samcole.me>
2025-02-25 11:59:32 +0000

    add stub code to `main.py` to demonstrate module usage

--- sendlog/main.py
+++ sendlog/main.py
@@ -1,11 +1,29 @@
-from plugin import Log, Rule, Transformation
 from utils.config_handler import ConfigHandler
-
-
-CONFIG_PATH = "sendlog.yml"
+from utils.workflow_manager import WorkflowManager
+from log_monitor import LogMonitor
 
 def main():
-    config = ConfigHandler(CONFIG_PATH)
+    # Load config into ConfigHandler
+    config_handler = ConfigHandler("sendlog.yml")
+    
+    # Create WorkflowManager
+    workflow_manager = WorkflowManager()
+
+    # Load destinations from config
+    for data in config_handler.destinations():
+        workflow_manager.load_destinations(*data)
+
+    # Load file workflows from config
+    for data in config_handler.files():
+        workflow_manager.load_workflow(*data)
+    
+    # Start file monitoring
+    paths = workflow_manager.get_paths()
+    log_monitor = LogMonitor(paths)
+    for path, msg in log_monitor.monitor():
+        print(f"{path}: {msg}")
+        # workflow_manager.get_workflow(path).execute(msg)
+
 
 if __name__ == "__main__":
     main()
