27de4a64cc3b5f30028b24a0b1ba583abdcd723e
samcole8 <sam@samcole.me>
2025-03-02 16:47:29 +0000

    refactor `workflow_manager.py`

--- sendlog/workflow_manager.py
+++ sendlog/workflow_manager.py
@@ -19,37 +19,38 @@ class WorkflowManager:
     def load_workflow(self, file_path, plugin_name, logtype_name, rule_name, transformer_name, destination_name):
         """Load a workflow based on the given parameters."""
 
-        # Set or load Log object
-        if file_path not in self._workflows.keys():
-            current_plugin = import_plugin(FILE_PLUGIN_RPATH + "." + plugin_name)
-            current_log = resolve_class(current_plugin, logtype_name)
-            logtype_node = current_log()
-            self._workflows[file_path] = logtype_node
-        else:
-            logtype_node = self._workflows[file_path]
-
-        # Set or load Rule objects
-        full_rule_name = repr(logtype_node) + "." + rule_name
-        if full_rule_name not in logtype_node:
-            current_rule = resolve_class(logtype_node, rule_name)
-            rule_node = current_rule()
-            logtype_node.add_child(rule_node)
-        else:
-            rule_node = next((child for child in logtype_node.children if repr(child) == full_rule_name), None)
-
-        # Set or load transformer objects
-        full_transformer_name = repr(rule_node) + "." + transformer_name
-        if full_transformer_name not in rule_node:
-            current_transformer = resolve_class(rule_node, transformer_name)
-            transformer_node = current_transformer()
-            rule_node.add_child(transformer_node)
-        else:
-            transformer_node = next((child for child in rule_node.children if repr(child) == full_transformer_name), None)
+        def _set_logtype(file_path, plugin_name, logtype_name):
+            if file_path not in self._workflows.keys():
+                current_plugin = import_plugin(FILE_PLUGIN_RPATH + "." + plugin_name)
+                current_log = resolve_class(current_plugin, logtype_name)
+                logtype_node = current_log()
+                self._workflows[file_path] = logtype_node
+            return self._workflows[file_path]
             
-        # Set or load Destinations
-        destination_class = self._destinations[destination_name]["class"]
-        destination_vars = self._destinations[destination_name]["vars"] or {}
-        transformer_node.add_child(destination_class(**destination_vars))
+        def _set_rule(logtype_node, rule_name):
+            full_rule_name = repr(logtype_node) + "." + rule_name
+            if full_rule_name not in logtype_node:
+                current_rule = resolve_class(logtype_node, rule_name)
+                rule_node = current_rule()
+                logtype_node.add_child(rule_node)
+            else:
+                rule_node = next((child for child in logtype_node.children if repr(child) == full_rule_name), None)
+            return rule_node
+
+        def _set_destination(transformer_node, destination_name):
+            # Set or load Destinations
+            destination_class = self._destinations[destination_name]["class"]
+            destination_vars = self._destinations[destination_name]["vars"] or {}
+            destination_node = destination_class(destination_name, **destination_vars)
+            transformer_node.add_child(destination_node)
+            return destination_node
+        
+        logtype_node = _set_logtype(file_path, plugin_name, logtype_name)
+        rule_node = _set_rule(logtype_node, rule_name)
+        transformer_node = _set_rule(rule_node, transformer_name) # Set transformer
+        destination_node = _set_destination(transformer_node, destination_name)
+
+        #print(f"\n{file_path}:\n{logtype_node}\n{rule_node}\n{transformer_node}\n{destination_node}")
 
     def load_destinations(self, plugin_name, endpoint_name, destination_name, destination_vars):
         endpoint_plugin = import_plugin(f"{ENDPOINT_PLUGIN_RPATH}.{plugin_name}")
@@ -57,7 +58,7 @@ class WorkflowManager:
         self._destinations[destination_name] = {"class": endpoint_class, "vars": destination_vars}
         if set(endpoint_class.required_vars) != set(destination_vars.keys()):
             raise EndpointVariableMismatchError(destination_name, endpoint_name, set(endpoint_class.required_vars), set(destination_vars.keys()))
-
+    
     def get_paths(self):
         return list(self._workflows.keys())
 
