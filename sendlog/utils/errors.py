aeac6bfedd2e02da85089baaaa27d74104e5d19d
samcole8 <sam@samcole.me>
2025-03-03 17:45:54 +0000

    add workflow pre-validation

--- sendlog/utils/errors.py
+++ sendlog/utils/errors.py
@@ -1,3 +1,17 @@
+# Workflow Errors
+
 class EndpointVariableMismatchError(Exception):
     def __init__(self, dest, endpoint, expected_vars, got_vars):
         super().__init__(f"Invalid configuration file: Endpoint plugin '{endpoint}' did not receive the expected variable(s) '{expected_vars}' from destination '{dest}'; got '{got_vars}' instead.")
+
+class PluginModuleNotFoundError(Exception):
+    def __init__(self, plugin):
+        super().__init__(f"Invalid configuration file: The referenced plugin module '{plugin}' does not exist.")
+
+class PluginClassNotFoundError(Exception):
+    def __init__(self, child, parent):
+        super().__init__(f"Invalid configuration file: The referenced plugin class '{child}' does not exist within the specified parent '{repr(parent)}'.")
+
+class DestinationUndefinedError(Exception):
+    def __init__(self, dest_name):
+        super().__init__(f"Invalid configuration file: The referenced destination '{dest_name}' is undefined.")
--- sendlog/workflow_manager.py
+++ sendlog/workflow_manager.py
@@ -1,15 +1,21 @@
 from importlib import import_module
 
-from utils.errors import EndpointVariableMismatchError
+from utils.errors import EndpointVariableMismatchError, PluginModuleNotFoundError, PluginClassNotFoundError, DestinationUndefinedError
 
 FILE_PLUGIN_RPATH = "plugins.workflows"
 ENDPOINT_PLUGIN_RPATH = "plugins.endpoints"
 
 def resolve_class(parent, class_name):
-    return getattr(parent, class_name)
+    try:
+        return getattr(parent, class_name)
+    except AttributeError as e:
+        raise PluginClassNotFoundError(class_name, parent) from e
         
 def import_plugin(plugin):
-    return import_module(plugin)
+    try:
+        return import_module(plugin)
+    except ModuleNotFoundError as e:
+        raise PluginModuleNotFoundError(plugin) from e
 
 class WorkflowManager:
     def __init__(self):
@@ -17,45 +23,55 @@ class WorkflowManager:
         self._destinations = {}
 
     def load_workflow(self, file_path, plugin_name, logtype_name, rule_name, transformer_name, destination_name):
-        """Load a workflow based on the given parameters."""
-
+        """Idempotently load elements of a workflow based on the given parameters."""
         def _set_logtype(file_path, plugin_name, logtype_name):
+            """Idempotently load a LogType."""
             if file_path not in self._workflows.keys():
-                current_plugin = import_plugin(FILE_PLUGIN_RPATH + "." + plugin_name)
-                current_log = resolve_class(current_plugin, logtype_name)
-                logtype_node = current_log()
-                self._workflows[file_path] = logtype_node
+                # Import plugin
+                plugin = import_plugin(f"{FILE_PLUGIN_RPATH}.{plugin_name}")
+                # Resolve and instantiate LogType object
+                LogType = resolve_class(plugin, logtype_name)
+                self._workflows[file_path] = LogType()
             return self._workflows[file_path]
             
         def _set_rule(logtype_node, rule_name):
-            full_rule_name = repr(logtype_node) + "." + rule_name
-            if full_rule_name not in logtype_node:
-                current_rule = resolve_class(logtype_node, rule_name)
-                rule_node = current_rule()
+            """Idempotently load a Rule or Transformer."""
+            rule_id = f"{logtype_node}.{rule_name}"
+            if rule_id not in logtype_node:
+                Rule = resolve_class(logtype_node, rule_name)
+                rule_node = Rule()
                 logtype_node.add_child(rule_node)
             else:
-                rule_node = next((child for child in logtype_node.children if repr(child) == full_rule_name), None)
+                rule_node = next((child for child in logtype_node.children if child == rule_id), None)
             return rule_node
 
         def _set_destination(transformer_node, destination_name):
-            # Set or load Destinations
-            destination_class = self._destinations[destination_name]["class"]
+            """Load an Endpoint."""
+            # Extract Endpoint class and destination variables
+            try:
+                Endpoint = self._destinations[destination_name]["class"]
+            except KeyError as e:
+                raise DestinationUndefinedError(destination_name) from e
             destination_vars = self._destinations[destination_name]["vars"] or {}
-            destination_node = destination_class(destination_name, **destination_vars)
+            # Instantiate the destination object
+            destination_node = Endpoint(destination_name, **destination_vars)
             transformer_node.add_child(destination_node)
             return destination_node
-        
+
         logtype_node = _set_logtype(file_path, plugin_name, logtype_name)
         rule_node = _set_rule(logtype_node, rule_name)
         transformer_node = _set_rule(rule_node, transformer_name) # Set transformer
         destination_node = _set_destination(transformer_node, destination_name)
 
-        #print(f"\n{file_path}:\n{logtype_node}\n{rule_node}\n{transformer_node}\n{destination_node}")
+        #print(f"\n\nReceived {file_path}, {plugin_name}, {logtype_name}, {rule_name}, {transformer_name}, {destination_name}")
+        #print(f"\nWorkflow loaded:\n  Path:        {file_path}\n  LogType:     {logtype_node}\n  Rule:        {rule_node}\n  Transformer: {transformer_node}\n  Destination: {destination_node}")
 
     def load_destinations(self, plugin_name, endpoint_name, destination_name, destination_vars):
         endpoint_plugin = import_plugin(f"{ENDPOINT_PLUGIN_RPATH}.{plugin_name}")
         endpoint_class = resolve_class(endpoint_plugin, endpoint_name)
         self._destinations[destination_name] = {"class": endpoint_class, "vars": destination_vars}
+        if destination_vars is None:
+            destination_vars = {None:None}
         if set(endpoint_class.required_vars) != set(destination_vars.keys()):
             raise EndpointVariableMismatchError(destination_name, endpoint_name, set(endpoint_class.required_vars), set(destination_vars.keys()))
     
