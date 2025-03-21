957e0ecf482fb483f5de25323078412d0f274dcd
samcole8 <sam@samcole.me>
2025-03-21 14:34:39 +0000

    Remove unnecessary console output from Telegram plugin

--- sendlog/plugins/channels/telegram.py
+++ sendlog/plugins/channels/telegram.py
@@ -1,4 +1,5 @@
 from plugin import Channel
+
 import requests
 
 class Telegram(Channel):
@@ -8,5 +9,3 @@ class Telegram(Channel):
         url = f"https://api.telegram.org/bot{self.token}/sendMessage"
         params = {"chat_id": self.chat_id, "text": msg}
         response = requests.post(url, params=params,)
-        if response.status_code == 200:
-            print(f"{msg} -> {self}")
